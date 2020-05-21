21e6744d7506399d259dc06113ca8a43180c0ac1
mari_s <ms.mari.suzuki@gmail.com>
2020-05-21 21:24:51 +0900

    in progress 196. Setup Sample Todo List Application changed settings in edit configuration set Java 8 for project structure replaced GridPane with BorderPane

--- src/com/dogeshibeee/todolist/Main.java
+++ src/com/dogeshibeee/todolist/Main.java
@@ -11,8 +11,8 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("mainwindow.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setTitle("Todo List");
+        primaryStage.setScene(new Scene(root, 900, 500));
         primaryStage.show();
     }
 
