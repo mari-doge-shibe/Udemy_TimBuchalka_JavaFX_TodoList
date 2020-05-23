89c56baa0d943c991a282e42c94f6c787b4c233d
mari_s <ms.mari.suzuki@gmail.com>
2020-05-23 20:58:17 +0900

    in progress 200. Singletons added IOException handling

--- src/com/dogeshibeee/todolist/datamodel/TodoData.java
+++ src/com/dogeshibeee/todolist/datamodel/TodoData.java
@@ -1,5 +1,14 @@
 package com.dogeshibeee.todolist.datamodel;
 
+import javafx.collections.FXCollections;
+
+import java.io.BufferedReader;
+import java.io.IOError;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
 
@@ -25,4 +34,34 @@ public class TodoData {
     public void setTodoItems(List<TodoItem> todoItems) {
         this.todoItems = todoItems;
     }
+
+    public void loadTodoItems() throws IOException {
+
+        todoItems = FXCollections.observableArrayList();
+        Path path = Paths.get(filename);
+        BufferedReader br = Files.newBufferedReader(path);
+
+        String input;
+
+        try {
+            while ((input = br.readLine()) != null) {
+                String[] itemPieces = input.split("\t");
+
+                String shortDescription = itemPieces[0];
+                String details = itemPieces[1];
+                String dateString = itemPieces[2];
+
+                LocalDate date = LocalDate.parse(dateString, formatter);
+                TodoItem todoItem = new TodoItem(shortDescription, details, date);
+                todoItems.add(todoItem);
+            }
+        } finally {
+            if(br != null) {
+                br.close();
+            }
+        }
+    }
+
+    
+
 }
