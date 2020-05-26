55bbe8391eebf3515f2242fff2969e5974d44120
mari_s <ms.mari.suzuki@gmail.com>
2020-05-26 20:54:42 +0900

    in progress 205. Data Binding and Observable deleted the code that is not needed for data binding

--- src/com/dogeshibeee/todolist/Controller.java
+++ src/com/dogeshibeee/todolist/Controller.java
@@ -73,9 +73,9 @@ public class Controller {
         if(result.isPresent() && result.get() == ButtonType.OK) {
             DialogController controller = fxmlLoader.getController();
             TodoItem newItem = controller.processResults();
-            todoListView.getItems().setAll(TodoData.getInstance().getTodoItems());
+
             todoListView.getSelectionModel().select(newItem);
-            System.out.println("OK pressed");
+
         } else {
             System.out.println("Cancel pressed");
         }
